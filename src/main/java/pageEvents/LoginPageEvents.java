b5298289a582f51819178b10ccff6f412bbe5010
Anil Magendra <anil.magene@gmail.com>
2021-02-02 22:25:49 +1000

    framework utilities

---
+++ src/main/java/pageEvents/LoginPageEvents.java
@@ -0,0 +1,5 @@
+package main.java.pageEvents;
+
+public class LoginPageEvents {
+
+}
---
+++ src/main/java/pageObjects/HomePageElements.java
@@ -0,0 +1,5 @@
+package main.java.pageObjects;
+
+public interface HomePageElements {
+    String homePageLabel = "//span[text() = 'Hello, Sign in']";
+}
